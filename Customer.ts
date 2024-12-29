b0b5e7f5ba479f7d47c7aa1ae50e4251f3c4083c
FahadAminShovon <shovonfahad991@gmail.com>
2024-12-29 15:37:05 +0600

    update customer rent vehicle method

--- Customer.ts
+++ Customer.ts
@@ -1,30 +1,29 @@
 import { Vehicle } from './Vehicle';
 
 export class Customer {
-  private _firstName: string;
-  private _lastName: string;
+  #firstName: string;
+  #lastName: string;
+  #rentedVehicles: Vehicle[] = [];
 
   constructor(firstName: string, lastName: string) {
-    this._firstName = firstName;
-    this._lastName = lastName;
+    this.#firstName = firstName;
+    this.#lastName = lastName;
   }
 
   public getFirstName(): string {
-    return this._firstName;
+    return this.#firstName;
   }
 
   public getLastName(): string {
-    return this._lastName;
+    return this.#lastName;
   }
 
   public rentVehicle(vehicle: Vehicle, from: Date, till: Date): void {
-    const status = vehicle.checkStatus(from, till);
-    if (status === 'Maintenance') {
-      throw new Error('Vehicle is in maintenance');
+    try {
+      vehicle.rentVehicle(from, till);
+      this.#rentedVehicles.push(vehicle);
+    } catch (e) {
+      console.log(e.message);
     }
-    if (status === 'Rented') {
-      throw new Error('Vehicle is already rented');
-    }
-    vehicle.rentVehicle(from, till);
   }
 }
